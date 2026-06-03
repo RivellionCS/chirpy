1a475bbe263678c6bba450a75b70c0a992a797d0
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-03 03:12:21 -0300

    create myHandler

--- main.go
+++ main.go
@@ -21,3 +21,9 @@ func buildAndStartServer() {
 	log.Printf("Serving files from %s on port: %s", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
