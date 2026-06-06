749c8788441f74e5c37e5b5e3f4826ee39e524c9
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-06 16:54:15 -0300

    create respondWithJSON function

--- main.go
+++ main.go
@@ -105,3 +105,15 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	w.WriteHeader(code)
 	w.Write(dat)
 }
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
