dedc802ab9c8689a3049ec90c290a87ea40c0551
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-08 01:26:46 -0300

    add fields to table

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,5 +1,10 @@
 -- +goose Up
-CREATE TABLE users;
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+);
 
 -- +goose Down
 DROP TABLE users;
