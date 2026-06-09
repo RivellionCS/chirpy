30c03af31a6c620dc507d529f571a46615e37ba7
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-09 04:31:12 -0300

    add the ability to delete all users

--- main.go
+++ main.go
@@ -85,6 +85,7 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.databaseQueries.DeleteUsers(r.Context())
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	cfg.fileserverHits.Store(0)
