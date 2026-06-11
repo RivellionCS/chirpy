34763c6560775b71547e22e89b1bbaa609b0eb65
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-11 03:02:07 -0300

    remove error logging

--- internal/auth/hashing.go
+++ internal/auth/hashing.go
@@ -1,25 +1,13 @@
 package auth
 
 import (
-	"log"
-
 	"github.com/alexedwards/argon2id"
 )
 
 func HashPassword(password string) (string, error) {
-	hashedPassword, err := argon2id.CreateHash(password, argon2id.DefaultParams)
-	if err != nil {
-		log.Printf("error hashing password: %s", err)
-		return "", err
-	}
-	return hashedPassword, nil
+	return argon2id.CreateHash(password, argon2id.DefaultParams)
 }
 
 func CheckPasswordHash(password, hash string) (bool, error) {
-	check, err := argon2id.ComparePasswordAndHash(password, hash)
-	if err != nil {
-		log.Printf("error checking password and hash: %s", err)
-		return false, err
-	}
-	return check, nil
+	return argon2id.ComparePasswordAndHash(password, hash)
 }
