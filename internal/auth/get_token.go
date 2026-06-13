d89874e98459b4a320da8db7ca5725170f7d78db
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-13 18:26:16 -0300

    add empty header check and fix trimPrefix

--- internal/auth/get_token.go
+++ internal/auth/get_token.go
@@ -9,12 +9,16 @@ import (
 func GetBearerToken(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 
+	if authHeader == "" {
+		return "", fmt.Errorf("Header is empty")
+	}
+
 	if !strings.HasPrefix(authHeader, "Bearer ") {
 		return "", fmt.Errorf("Authorization header must start with 'Bearer '")
 	}
 
-	token := strings.TrimSpace(strings.TrimPrefix(authHeader, "Bearer"))
-	
+	token := strings.TrimSpace(strings.TrimPrefix(authHeader, "Bearer "))
+
 	if token == "" {
 		return "", fmt.Errorf("Error empty token")
 	}
