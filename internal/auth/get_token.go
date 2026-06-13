a2a676639f4ef89060b2ba06bffb6d4ad04b28fb
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-13 18:16:52 -0300

    fix bug

--- internal/auth/get_token.go
+++ internal/auth/get_token.go
@@ -8,9 +8,16 @@ import (
 
 func GetBearerToken(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
-	strippedHeader := strings.TrimSpace(strings.TrimPrefix(authHeader, "Bearer"))
-	if strippedHeader == "" {
+
+	if !strings.HasPrefix(authHeader, "Bearer ") {
+		return "", fmt.Errorf("Authorization header must start with 'Bearer '")
+	}
+
+	token := strings.TrimSpace(strings.TrimPrefix(authHeader, "Bearer"))
+	
+	if token == "" {
 		return "", fmt.Errorf("Error empty token")
 	}
-	return strippedHeader, nil
+
+	return token, nil
 }
