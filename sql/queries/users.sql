09a7816547ecaaab8c0158ae80855c00bf1c914d
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-16 20:02:07 -0300

    query for updating user

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -15,3 +15,10 @@ DELETE FROM users;
 -- name: GetUserByEmail :one
 SELECT * FROM users
 WHERE email = $1;
+
+-- name: UpdateUserEmailAndPassword :exec
+UPDATE users
+SET updated_at = NOW(),
+    email = $2,
+    hashed_password = $3
+WHERE id = $1;
